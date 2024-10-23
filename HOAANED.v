fe54c320388f66d8aaeeca3c5ff2a26bc23a6bfa
Nakul Koshal <nakulkoshal4@gmail.com>
2024-10-24 01:35:47 +0530

    Update HOAANED.v

--- HOAANED.v
+++ HOAANED.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 28.09.2024 23:54:16
-// Design Name: 
-// Module Name: HOAANED
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 // Hardware Optimized Approximate Adder with a Near-normal Error Distribution
 module HOAANED
