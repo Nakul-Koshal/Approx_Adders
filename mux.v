9ce5a7118d428e10e19037ee81a1c96189d752e4
Nakul Koshal <nakulkoshal4@gmail.com>
2024-10-24 01:37:16 +0530

    Update mux.v

--- mux.v
+++ mux.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 29.09.2024 01:13:24
-// Design Name: 
-// Module Name: mux
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module mux
